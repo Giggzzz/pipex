5d04bca480f713335935671b2178412ce32f4fbc
giggz <giggz@wsl>
2022-03-18 02:33:46 +0100

    progress

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: gudias <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 17:26:56 by gudias            #+#    #+#             */
-/*   Updated: 2022/03/17 17:32:22 by gudias           ###   ########.fr       */
+/*   Updated: 2022/03/18 02:32:52 by gudias           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,42 +16,61 @@ int	main(int argc, char **argv, char **envp)
 {
 	int		id;
 	int		pipe1[2];
+	int		pipe2[2];
 	char	r;
-	
-	check_args(argc, argv, envp);
+	int	infile;
+	int	outfile;
 
-	if (pipe(pipe1) == -1)
+	//check_args(argc, argv, envp);
+	infile = open(argv[1], O_WRONLY | O_CREAT, 0644);	
+	outfile = open(argv[4], O_WRONLY | O_CREAT, 0644);
+	if (!outfile)
+		err_quit(3);
+	if (pipe(pipe1) == -1 || pipe(pipe2) == -1)
 		err_quit(5);
 
 	id = fork();
 	if (id == -1)
 		err_quit(6);
 
-	char	*av[5];
-	//av[0] = NULL;
-
-	av[0] = "/sbin/ping";
-	av[1] = "www.google.com";
-	av[2] = "-c";
-	av[3] = "4";
-	av[4] = NULL;
+	char	*av1[3];
+	av1[0] = "/usr/bin/cat";
+	av1[1] = "infile";
+	//1av[2] = "-c";
+	//1av[3] = "4";
+	av1[2] = NULL;
+	char	*av2[3];
+	av2[0] = "/usr/bin/grep";
+	av2[1] = "aigh";
+	av2[2] = NULL;
 	if (id == 0)
 	{
 		close(pipe1[0]);
 		dup2(pipe1[1],1);
 		close(pipe1[1]);
-		execve("/sbin/ping", av, envp);
+		execve(av[0], av, envp);
 		err_quit(7);	
 	}
 	else
 	{
-		close(pipe1[1]);
+		//close(pipe1[1]);
 	//	wait(NULL);
 		while (read(pipe1[0], &r, 1)) 
-			write(pipe1[1], &r, 1);
+			write(pipe2[1], &r, 1);
+		
+		id = fork();
+		if (id == -1)
+			err_quit(6);
 
+		if (id == 0)
+		{
+			
+		}
+		else
+		{
 
-		//write(1, "\n", 1);
+		}
+
 		close(pipe1[0]);
 	}
 	return (0);
